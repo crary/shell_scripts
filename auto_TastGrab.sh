44c141a6192e727d3141aded0cc7e79630530904
David Crary <22047656+crary@users.noreply.github.com>
2023-02-03 10:37:13 -0800

    Update auto_TastGrab.sh
    Added ability to push scripts back into cros_sdk with an array of files.

--- auto_TastGrab.sh
+++ auto_TastGrab.sh
@@ -8,33 +8,46 @@ tastPath=/home/oem/chromiumos/src/platform/tast-tests/local_tests
 crosRoot=/root/new_grab
 autoGrab=/home/oem/Desktop/tast_files/auto_grab
 pull="${1:-pull}"
+fileArray=( "quick_lvplay.go" "quick_wallplay.go" "quick_pltplay.go" "quick_pltplay2.go" "quick_gaia.go" "quick_gaiaplay.go" )
 
 if [[ "$pull" == "pull" ]]; then
+
 $cros_sdk << EOF
-## LVP Tast File
-cd "${tastPath}"/filemanager
-sudo cp quick_lvp.go quick_lvplay.go "${crosRoot}"
+	## LVP Tast File
+	cd "${tastPath}"/filemanager
+	sudo cp quick_lvplay.go "${crosRoot}"
 
-## IDO/S0ix Tast File
-cd "${tastPath}"/wallpaper
-sudo cp quick_wallplay.go "${crosRoot}"
+	## IDO/S0ix Tast File
+	cd "${tastPath}"/wallpaper
+	sudo cp quick_wallplay.go "${crosRoot}"
 
-## PLT Tast File
-cd "${tastPath}"/example
-sudo cp quick_pltplay.go quick_pltplay2.go "${crosRoot}"
+	## PLT Tast File
+	cd "${tastPath}"/example
+	sudo cp quick_pltplay.go quick_pltplay2.go "${crosRoot}"
 
-## GMeet Tast File
-cd "${tastPath}"/login
-sudo cp quick_gaia.go quick_gaiaplay.go "${crosRoot}"
+	## GMeet Tast File
+	cd "${tastPath}"/login
+	sudo cp quick_gaia.go quick_gaiaplay.go "${crosRoot}"
 
-## ADB Connect Tast File
-#cd "${tastPath}"/login
-#sudo cp quick_adb.go quick_adb2.go "${crosRoot}"
+	## ADB Connect Tast File
+	#cd "${tastPath}"/login
+	#sudo cp quick_adb.go quick_adb2.go "${crosRoot}"
 EOF
-
-fi
 
 sudo mv ~/PNP_June7/chroot"${crosRoot}"/* "${autoGrab}"
 sudo chown oem:oem "${autoGrab}"/quick*
+
+elif [[ "$1" == "push" ]]; then
+	# If $2 is empty push all tast files
+	if [[ -z "$2" ]]; then
+		echo "Pushing all Tast files"
+		sleep 1s
+		echo "${fileArray[@]}"
+		## Move file in array to cros_sdk intermidiary directory
+		sudo mv "${autoGrab}"/"${fileArray[@]}" ~/PNP_June7/chroot"${crosRoot}"
+	fi
+
+fi
+
 
 exit
